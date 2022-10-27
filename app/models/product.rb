765e4fb09dea4fb3fa593cf1adfa55f1fda10d7d
slira4 <slira4@uc.cl>
2022-10-26 23:13:23 -0300

    validations for product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,10 +2,10 @@
 
 class Product < ApplicationRecord
   validates :name, presence: true
-  # validates :price, presence: true
-  # validates :price, numericality: {only_integer: true}
-  # validates :category, presence: true
-  # validates :size, inclusion: { in: %w(Bebestible Comestible Souvenir),
-  #   message: "%{value} is not a valid category" }
-  # validates :weight_volume, numericality: {only_integer: true}
+  validates :price, presence: true
+  validates :price, numericality: { only_integer: true }
+  validates :category, presence: true
+  validates :size, inclusion: { in: %w[Bebestible Comestible Souvenir],
+                                message: '%<value>s is not a valid category' }
+  validates :weight_volume, numericality: { only_integer: true }
 end
