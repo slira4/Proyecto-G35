d288170dc46386088dfab59f2154f185b5bce25a
dsilvc <dsilvc@uc.cl>
2022-10-28 20:28:34 -0300

    60% movies controller

--- test/controllers/movie_controller_test.rb
+++ test/controllers/movie_controller_test.rb
@@ -14,4 +14,34 @@ class MovieControllerTest < ActionDispatch::IntegrationTest
     get movie_new_url
     assert_response :success
   end
+
+  #NOT WORKING 
+
+  # test 'should post a new movie' do
+  #   post create_movie_url,
+  #   params: { movie: { title: 'Batman', image: ''} }
+  #   assert_redirected_to '/movie/new'
+  # end
+   
+
+  test 'should not post a new movie' do
+      post create_movie_url,
+      params: { movie: { title: '', image: ''} }
+      assert_redirected_to '/movie/new'
+  end
+
+  #ESTE PARA ASIGNAR HORARIOS
+
+  # test 'should assign a movie time' do
+  #   post new_movie_time_url,
+  #   params: { movie_time_params: { movie_time: 'TANDA', movie_id: '1', time: , date_start: , date_end: , room: } }
+  # end
+
+  #ESTE PARA FILTRAR POR FECHA
+
+  # test 'should filter by date' do 
+  #   get movies_by_date, 
+  #   params: { date: '2000-11-12' }
+  #   assert_response :success
+  # end
 end
