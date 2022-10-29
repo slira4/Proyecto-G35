7b9b63ded7bab85f99d92e70b5c137cdb9bcb2b9
slira4 <slira4@uc.cl>
2022-10-28 21:53:12 -0300

    98% controllers coverage, 100% models coverage

--- test/controllers/movie_controller_test.rb
+++ test/controllers/movie_controller_test.rb
@@ -17,11 +17,11 @@ class MovieControllerTest < ActionDispatch::IntegrationTest
 
   #NOT WORKING 
 
-  # test 'should post a new movie' do
-  #   post create_movie_url,
-  #   params: { movie: { title: 'Batman', image: ''} }
-  #   assert_redirected_to '/movie/new'
-  # end
+  test 'should post a new movie' do
+    post create_movie_url,
+         params: { movie: { title: 'Batman', image: 'string'} }
+    assert_redirected_to '/movie/new'
+  end
    
 
   test 'should not post a new movie' do
@@ -32,16 +32,18 @@ class MovieControllerTest < ActionDispatch::IntegrationTest
 
   #ESTE PARA ASIGNAR HORARIOS
 
-  # test 'should assign a movie time' do
-  #   post new_movie_time_url,
-  #   params: { movie_time_params: { movie_time: 'TANDA', movie_id: '1', time: , date_start: , date_end: , room: } }
-  # end
+  test 'should assign a movie time' do
+    post new_movie_time_url,
+         params: { movie_time:
+           { movie_time:'TANDA', movie_id: '1', time: nil ,
+             date_start: nil , date_end: nil, room: nil } }
+  end
 
   #ESTE PARA FILTRAR POR FECHA
 
-  # test 'should filter by date' do 
-  #   get movies_by_date, 
-  #   params: { date: '2000-11-12' }
-  #   assert_response :success
-  # end
+  test 'should filter by date' do 
+    get movies_by_date_url, 
+    params: { date: '2000-11-12' }
+    assert_response :success
+  end
 end
