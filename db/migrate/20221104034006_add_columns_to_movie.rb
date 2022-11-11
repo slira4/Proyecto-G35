bc15ab18d96510e0d0629442e229dca1d12d5e3c
dsilvc <dsilvc@uc.cl>
2022-11-11 12:03:56 -0300

    Testing migrations

--- db/migrate/20221104034006_add_columns_to_movie.rb
+++ db/migrate/20221104034006_add_columns_to_movie.rb
@@ -3,9 +3,9 @@
 class AddColumnsToMovie < ActiveRecord::Migration[7.0]
   def change
     change_table :movies, bulk: true do |t|
-      t.column :movies, :age, :integer, null: false
-      t.column :movies, :sucursal, :string, null: false
-      t.column :movies, :languaje, :string, null: false
+      t.integer :movies, :age
+      t.string :movies, :sucursal
+      t.string :movies, :languaje
     end
   end
 end
