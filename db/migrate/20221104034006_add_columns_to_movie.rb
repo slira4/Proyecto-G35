576288d97734f7f6fa6f15ebe72bc00b4a74dabe
slira4 <slira4@uc.cl>
2022-11-11 12:45:16 -0300

    Converting dos to unix format

--- db/migrate/20221104034006_add_columns_to_movie.rb
+++ db/migrate/20221104034006_add_columns_to_movie.rb
@@ -3,9 +3,9 @@
 class AddColumnsToMovie < ActiveRecord::Migration[7.0]
   def change
     change_table :movies, bulk: true do |t|
-      t.column :movies, :age, :integer
-      t.column :movies, :sucursal, :string
-      t.column :movies, :languaje, :string
+      t.integer :age
+      t.string :sucursal
+      t.string :languaje
     end
   end
 end
