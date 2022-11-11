f877ba91b5852be16928816208851dd07392739e
dsilvc <dsilvc@uc.cl>
2022-11-11 12:29:40 -0300

    Rubocop offenses

--- db/migrate/20221104034006_add_columns_to_movie.rb
+++ db/migrate/20221104034006_add_columns_to_movie.rb
@@ -2,10 +2,8 @@
 
 class AddColumnsToMovie < ActiveRecord::Migration[7.0]
   def change
-    change_table :movies, bulk: true do |t|
-      t.column :movies, :age, :integer
-      t.column :movies, :sucursal, :string
-      t.column :movies, :languaje, :string
-    end
+    add_column :movies, :age, :integer
+    add_column :movies, :sucursal, :string
+    add_column :movies, :languaje, :string
   end
 end
